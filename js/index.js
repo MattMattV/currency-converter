caa4a6375eb434c31f7d19cda2d4f2286842bf5d
Matthieu Vion <mvion@trace-software.com>
2017-01-22 09:11:16 +0100

    it's working !

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 let root = new Vue({
     el: '#app',
     data: {
-        change: 1.14466106,
-        euros: null,
-        pounds: null
+        changeGBPToEUR: 1.14466106,
+        changeEURToGBP: 0.883888426,
+        euros: 0,
+        pounds: 0
     },
     methods: {
+        convertToEuros: function () {
+            this.euros = this.changeGBPToEUR * this.pounds;
+            this.euros.toFixed(2);
+        },
+
+        convertToPounds: function () {
+            this.pounds = this.changeEURToGBP * this.euros;
+            this.pounds.toFixed(2);
+        },
+
         convert: function () {
-            this.euros = this.change * this.pounds
+            this.convertToEuros();
+            this.convertToPounds();
         }
     }
 })
